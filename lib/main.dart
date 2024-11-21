6e254b90776017670770c4933c80dde070d9a2d1
ZakSlinin <156526564+ZakSlinin@users.noreply.github.com>
2024-11-21 17:51:06 +0300

    add Login page

--- lib/main.dart
+++ lib/main.dart
@@ -19,12 +19,18 @@ class AuthorsRouteApp extends StatelessWidget {
             color: Color.fromARGB(255, 118, 118, 118),
             fontSize: 25,
             fontWeight: FontWeight.bold,
-
+          ),
+          bodyLarge: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.bold
           ),
         ),
+        appBarTheme: AppBarTheme(iconTheme: IconThemeData(color: Colors.white))
       ),
       routes: {
         '/': (context) => const AuthorsRouteReg(),
+        '/login': (context) => const AuthorsRouteLogin()
       },
     );
   }
@@ -48,16 +54,16 @@ class AuthorsRouteReg extends StatelessWidget {
                 padding: const EdgeInsets.fromLTRB(16, 16, 16, 16 ),
                 child: Column(
                   children: <Widget>[
-                    const Text(
+                    Text(
                       'Войти или зарегестрироваться',
-                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+                      style: theme.textTheme.bodyLarge,
                     ),
                     const SizedBox(height: 20),
                     TextFormField(
                       decoration: InputDecoration(
                           labelText: 'Имя',
                           labelStyle: theme.textTheme.bodyMedium,
-                          border: OutlineInputBorder(),
+                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
                           fillColor: const Color.fromARGB(255, 217, 217, 217),
                           filled: true
                       ),
@@ -67,7 +73,7 @@ class AuthorsRouteReg extends StatelessWidget {
                       decoration: InputDecoration(
                           labelText: 'Пароль',
                           labelStyle: theme.textTheme.bodyMedium,
-                          border: OutlineInputBorder(),
+                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
                           fillColor: const Color.fromARGB(255, 217, 217, 217),
                           filled: true
                       ),
@@ -77,14 +83,99 @@ class AuthorsRouteReg extends StatelessWidget {
                       decoration: InputDecoration(
                           labelText: 'Пароль',
                           labelStyle: theme.textTheme.bodyMedium,
-                          border: OutlineInputBorder(),
+                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
                           fillColor: const Color.fromARGB(255, 217, 217, 217),
                           filled: true
                       ),
                     ),
                     // TODO: Checking for an empty field
                     const SizedBox(height: 16),
+                    Container(
+                      height: 50,
+                      width: 390,
+                      child: DecoratedBox(
+                          decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(20)),
+                          child: Center(child: Text('Зарегестрироваться', style: theme.textTheme.bodyLarge))
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    InkWell(
+                      child: Container(
+                        height: 50,
+                        width: 390,
+                        child: Center(child: Text('У меня есть аккаунт!', style: TextStyle(color: Colors.white, fontWeight: FontWeight.normal))),
+                      ),
+                      onTap: () {
+                        Navigator.of(context).pushNamed('/login');
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
+class AuthorsRouteLogin extends StatelessWidget {
+  const AuthorsRouteLogin ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme  = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black, title: Text('Авторизация', style: theme.textTheme.bodyLarge,),
+      ),
+      body: DecoratedBox(
+        decoration: const BoxDecoration(
+            image: DecorationImage(image: AssetImage('assets/jpg/bg.jpg'), fit: BoxFit.cover)
+        ),
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(1, 180, 1, 2),
+          child: SafeArea(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16, 16, 16, 16 ),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'Войти или зарегестрироваться',
+                      style: theme.textTheme.bodyLarge,
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          labelText: 'Имя',
+                          labelStyle: theme.textTheme.bodyMedium,
+                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+                          fillColor: const Color.fromARGB(255, 217, 217, 217),
+                          filled: true
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          labelText: 'Пароль',
+                          labelStyle: theme.textTheme.bodyMedium,
+                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+                          fillColor: const Color.fromARGB(255, 217, 217, 217),
+                          filled: true
+                      ),
+                    ),
+                    // TODO: Checking for an empty field
+                    const SizedBox(height: 16),
+                    Container(
+                      height: 50,
+                      width: double.infinity,
+                      child: DecoratedBox(
+                          decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(20)),
+                          child: Center(child: Text('Войти', style: theme.textTheme.bodyLarge))
+                      ),
+                    ),
                   ],
                 ),
               ),
